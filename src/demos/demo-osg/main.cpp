ca7cc0faa7a617607f623438d63c3d6d54de98de
Victoria Rudakova <vicrucann@gmail.com>
2015-11-16 09:18:44 -0500

    back to basic osg example

--- src/demos/demo-osg/main.cpp
+++ src/demos/demo-osg/main.cpp
@@ -1,110 +1,14 @@
 //#include <QApplication>
 //#include <QMainWindow>
 
-/*#include <osgDB/ReadFile>
+#include <osgDB/ReadFile>
 #include <osgViewer/Viewer>
 
 int main(int argc, char** argv)
 {
-    osg::ref_ptr<osg::Node> root = osgDB::readNodeFile("cessna.osg");
+    osg::ref_ptr<osg::Node> root = osgDB::readNodeFile("../../samples/cow.osgt");
     osgViewer::Viewer viewer;
     viewer.setSceneData(root.get());
     return viewer.run();
-
-    //QApplication qapp(argc, argv);
-
-    //QMainWindow qwin;
-    //qwin.show();
-
-    //return qapp.exec();
-}*/
-
-#include <osgDB/ReadFile>
-#include <osgUtil/Optimizer>
-#include <osgViewer/Viewer>
-
-
-#include <iostream>
-
-int main( int argc, char **argv )
-{
-    // use an ArgumentParser object to manage the program arguments.
-    osg::ArgumentParser arguments(&argc,argv);
-
-    // read the scene from the list of file specified commandline args.
-    osg::ref_ptr<osg::Node> loadedModel = osgDB::readNodeFiles(arguments);
-
-    // if not loaded assume no arguments passed in, try use default mode instead.
-    if (!loadedModel) loadedModel = osgDB::readNodeFile("../demo-osg/cow.osgt");
-
-    // if no model has been successfully loaded report failure.
-    if (!loadedModel)
-    {
-        std::cout << arguments.getApplicationName() <<": No data loaded" << std::endl;
-        return 1;
-    }
-
-    // construct the viewer.
-    osgViewer::Viewer viewer;
-
-    int xoffset = 40;
-    int yoffset = 40;
-
-    // left window + left slave camera
-    {
-        osg::ref_ptr<osg::GraphicsContext::Traits> traits = new osg::GraphicsContext::Traits;
-        traits->x = xoffset + 0;
-        traits->y = yoffset + 0;
-        traits->width = 600;
-        traits->height = 480;
-        traits->windowDecoration = true;
-        traits->doubleBuffer = true;
-        traits->sharedContext = 0;
-
-        osg::ref_ptr<osg::GraphicsContext> gc = osg::GraphicsContext::createGraphicsContext(traits.get());
-
-        osg::ref_ptr<osg::Camera> camera = new osg::Camera;
-        camera->setGraphicsContext(gc.get());
-        camera->setViewport(new osg::Viewport(0,0, traits->width, traits->height));
-        GLenum buffer = traits->doubleBuffer ? GL_BACK : GL_FRONT;
-        camera->setDrawBuffer(buffer);
-        camera->setReadBuffer(buffer);
-
-        // add this slave camera to the viewer, with a shift left of the projection matrix
-        viewer.addSlave(camera.get(), osg::Matrixd::translate(1.0,0.0,0.0), osg::Matrixd());
-    }
-
-    // right window + right slave camera
-    {
-        osg::ref_ptr<osg::GraphicsContext::Traits> traits = new osg::GraphicsContext::Traits;
-        traits->x = xoffset + 600;
-        traits->y = yoffset + 0;
-        traits->width = 600;
-        traits->height = 480;
-        traits->windowDecoration = true;
-        traits->doubleBuffer = true;
-        traits->sharedContext = 0;
-
-        osg::ref_ptr<osg::GraphicsContext> gc = osg::GraphicsContext::createGraphicsContext(traits.get());
-
-        osg::ref_ptr<osg::Camera> camera = new osg::Camera;
-        camera->setGraphicsContext(gc.get());
-        camera->setViewport(new osg::Viewport(0,0, traits->width, traits->height));
-        GLenum buffer = traits->doubleBuffer ? GL_BACK : GL_FRONT;
-        camera->setDrawBuffer(buffer);
-        camera->setReadBuffer(buffer);
-
-        // add this slave camera to the viewer, with a shift right of the projection matrix
-        viewer.addSlave(camera.get(), osg::Matrixd::translate(-1.0,0.0,0.0), osg::Matrixd());
-    }
-
-
-    // optimize the scene graph, remove redundant nodes and state etc.
-    osgUtil::Optimizer optimizer;
-    optimizer.optimize(loadedModel.get());
-
-    // set the scene to render
-    viewer.setSceneData(loadedModel.get());
-
-    return viewer.run();
 }
+
