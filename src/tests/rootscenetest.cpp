3fd0d14a9198ec44961232da4601c84a554bf7e0
Victoria Rudakova <vicrucann@gmail.com>
2015-11-25 13:21:49 -0500

    main cmake file updated to more generic and also so that to be used by test cmake file

---
+++ src/tests/rootscenetest.cpp
@@ -0,0 +1,11 @@
+#include "rootscenetest.h"
+
+#include <QtTest>
+
+void RootSceneTest::test()
+{
+    QVERIFY(true);
+}
+
+QTEST_MAIN(RootSceneTest)
+#include "rootscenetest.moc"
---
+++ src/tests/rootscenetest.h
@@ -0,0 +1,19 @@
+#ifndef TEST_ROOTSCENE
+#define TEST_ROOTSCENE
+
+#include <QObject>
+#include <QTest>
+
+class RootSceneTest : public QObject {
+    Q_OBJECT
+public:
+    RootSceneTest(){
+        ;
+    }
+protected:
+private:
+    void test();
+};
+
+#endif // TEST_ROOTSCENE
+
