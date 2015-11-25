f1e0c617f6e03a4cb143d2ef62c23a4ebd02cbb8
Victoria Rudakova <vicrucann@gmail.com>
2015-11-25 12:29:55 -0500

    root scene test file minimal demo

---
+++ src/tests/test_rootscene.cpp
@@ -0,0 +1,11 @@
+#include "test_rootscene.h"
+
+#include <QtTest/QtTest>
+
+void Test_RootScene::test()
+{
+    QVERIFY(true);
+}
+
+QTEST_MAIN(Test_RootScene)
+#include "test_rootscene.moc"
---
+++ src/tests/test_rootscene.h
@@ -0,0 +1,18 @@
+#ifndef TEST_ROOTSCENE
+#define TEST_ROOTSCENE
+
+#include <QObject>
+
+class Test_RootScene : public QObject {
+    Q_OBJECT
+public:
+    Test_RootScene(){
+        ;
+    }
+protected:
+private:
+    void test();
+};
+
+#endif // TEST_ROOTSCENE
+
