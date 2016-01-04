b2b185f54371cf364e031e4c60e872d313c73bf9
Victoria Rudakova <vicrucann@gmail.com>
2016-01-04 16:30:44 -0500

    re-arrangement of application class

---
+++ src/libGUI/DureuApplication.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include <QtWidgets>
+
+#include "DureuApplication.h"
+
+DureuApplication::DureuApplication(int &argv, char **argc):
+    QApplication(argv, argc)
+{}
+
+DureuApplication::~DureuApplication(){}
+
+bool DureuApplication::event(QEvent* event){
+    if (event->type() == QEvent::TabletEnterProximity || event->type() == QEvent::TabletLeaveProximity) {
+            bool active = event->type() == QEvent::TabletEnterProximity? 1 : 0;
+            std::cerr << "tablet active: " << active << std::endl;
+            emit sendTabletActivity(active);
+            return true;
+        }
+    return QApplication::event(event);
+}
+
---
+++ src/libGUI/DureuApplication.h
@@ -0,0 +1,20 @@
+#ifndef DUREUAPPLICATION_H
+#define DUREUAPPLICATION_H
+
+#include <QApplication>
+#include <QTabletEvent>
+
+class DureuApplication : public QApplication {
+    Q_OBJECT
+public:
+    DureuApplication(int& argv, char** argc);
+    ~DureuApplication();
+signals:
+    void sendTabletActivity(bool active);
+public slots:
+protected:
+    bool event(QEvent* event) Q_DECL_OVERRIDE;
+private:
+};
+
+#endif // DUREUAPPLICATION_H
