dfcfdb005c38f99a3383adf2eb217bbc9df8c980
Victoria Rudakova <vicrucann@gmail.com>
2016-01-19 09:19:56 -0500

    undo commands for canvas offset and rotate

--- src/libSGControls/EditCanvasCommand.cpp
+++ src/libSGControls/EditCanvasCommand.cpp
@@ -1,7 +1,55 @@
 #include "EditCanvasCommand.h"
 
-EditCanvasCommand::EditCanvasCommand()
+#include <QObject>
+
+EditCanvasOffsetCommand::EditCanvasOffsetCommand(entity::Canvas *canvas, const osg::Vec3f &translate, QUndoCommand *parent)
+    : QUndoCommand(parent)
+    , m_canvas(canvas)
+    , m_translate(translate)
+{
+    this->setText(QObject::tr("Offset %1")
+                  .arg(QString(m_canvas->getName().c_str())));
+}
+
+EditCanvasOffsetCommand::~EditCanvasOffsetCommand()
+{
+}
+
+void EditCanvasOffsetCommand::undo()
+{
+    m_canvas->translate(osg::Matrix::translate(-m_translate.x(), -m_translate.y(), -m_translate.z()));
+}
+
+void EditCanvasOffsetCommand::redo()
+{
+    m_canvas->translate(osg::Matrix::translate(m_translate.x(), m_translate.y(), m_translate.z()));
+}
+
+/* =====================*/
+
+EditCanvasRotateCommand::EditCanvasRotateCommand(entity::Canvas *canvas, const osg::Quat &rotate, QUndoCommand *parent)
+    : QUndoCommand(parent)
+    , m_canvas(canvas)
+    , m_rotate(rotate)
+{
+    this->setText(QObject::tr("Rotate %1")
+                  .arg(QString(m_canvas->getName().c_str())));
+}
+
+EditCanvasRotateCommand::~EditCanvasRotateCommand()
 {
 
 }
 
+void EditCanvasRotateCommand::undo()
+{
+    double angle;
+    osg::Vec3d axis;
+    m_rotate.getRotate(angle, axis);
+    m_canvas->rotate(osg::Matrix::rotate(-angle, axis));
+}
+
+void EditCanvasRotateCommand::redo()
+{
+    m_canvas->rotate(osg::Matrix::rotate(m_rotate));
+}
--- src/libSGControls/EditCanvasCommand.h
+++ src/libSGControls/EditCanvasCommand.h
@@ -3,11 +3,36 @@
 
 #include <QUndoCommand>
 
-class EditCanvasCommand : public QUndoCommand
+#include <osg/observer_ptr>
+
+#include "Canvas.h"
+
+class EditCanvasOffsetCommand : public QUndoCommand
 {
 public:
-    EditCanvasCommand();
+    EditCanvasOffsetCommand(entity::Canvas* canvas, const osg::Vec3f& translate, QUndoCommand* parent = 0);
+    ~EditCanvasOffsetCommand();
 
+    void undo() Q_DECL_OVERRIDE;
+    void redo() Q_DECL_OVERRIDE;
+
+protected:
+    osg::observer_ptr<entity::Canvas> m_canvas;
+    osg::Vec3f m_translate;
+};
+
+class EditCanvasRotateCommand : public QUndoCommand
+{
+public:
+    EditCanvasRotateCommand(entity::Canvas* canvas, const osg::Quat& rotate, QUndoCommand* parent = 0);
+    ~EditCanvasRotateCommand();
+
+    void undo() Q_DECL_OVERRIDE;
+    void redo() Q_DECL_OVERRIDE;
+
+protected:
+    osg::observer_ptr<entity::Canvas> m_canvas;
+    osg::Quat m_rotate;
 };
 
 #endif // EDITCANVASCOMMAND_H
