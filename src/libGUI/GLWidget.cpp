9e49458ab79511707649c5a4fa63ff7d9c8cb0d6
Victoria Rudakova <vicrucann@gmail.com>
2016-07-01 09:48:09 -0400

    dependencies added for glwidget.cpp

--- src/libGUI/GLWidget.cpp
+++ src/libGUI/GLWidget.cpp
@@ -16,6 +16,9 @@
 #include <osgGA/TrackballManipulator>
 
 #include "SceneState.h"
+#include "ViewerCommand.h"
+#include "Manipulator.h"
+#include "EventHandler.h"
 
 GLWidget::GLWidget(RootScene *root, QUndoStack *stack, QWidget *parent, Qt::WindowFlags f)
     : QOpenGLWidget(parent, f)
