0c963f7435512028b4c21ca98f1999e4d43c5505
Victoria Rudakova <vicrucann@gmail.com>
2016-11-18 14:33:21 -0500

    readme updated and typos are fixed

--- src/cherish/main.cpp
+++ src/cherish/main.cpp
@@ -20,10 +20,10 @@
  *
  * \section Description
  *
- * This documentation site covers the cherish API mainly. For the user documentation, try to check if [Cherish site](http://vicrucann.github.io/cherish/) is upgraded.
+ * This documentation site covers the cherish API mainly.
  *
- * To start, you may browse the content and hierarchy of `Classes`. For other non-API documentation files, check `Related Pages`.
- * The codes are separated into different folders: `cherish`, `libGUI`, `libSGControls`, `libSGEntities`, `libNumerics` and `tests`.
+ * To start with API, you may browse the content and hierarchy of `Classes`. The codes are separated into different folders: `cherish`, `libGUI`, `libSGControls`,
+ * `libSGEntities`, `libNumerics` and `tests`.
  *
  * * `cherish` folder contains `main.cpp`, as well as settings, utility functions, shader and image data.
  * * `libGUI` contains classes related to user interface, widgets and its content.
@@ -31,6 +31,12 @@
  * * `libSGEntities` contains geometry-based and group-based OpenSceneGraph elements that are elements of a scene graph.
  * * `libNumerics` contain outside numerical libraries and codes, e.g. curve fitting algorithm.
  * * `tests` contains batteries of different basic unit tests.
+ *
+ * Some other useful pages:
+ * * [Cherish scene graph](https://codedocs.xyz/vicrucann/cherish/md_docs_scene.html)
+ * * [Cherish README](https://codedocs.xyz/vicrucann/cherish/md_README.html)
+ * * [Cherish user site](http://vicrucann.github.io/cherish/) (is not up to date currently)
+ * * [Cherish license file](https://github.com/vicrucann/cherish/blob/develop/LICENSE)
  */
 
 int main(int argc, char** argv)
